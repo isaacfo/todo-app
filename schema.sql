55b51568f412c8379de4dc433cefcced12b6cf2d
Isaac Fonseca <isaacfo1010@gmail.com>
2018-11-05 10:46:25 -0500

    added db and reset-db scripts

---
+++ schema.sql
@@ -0,0 +1,13 @@
+--TODOs
+--name
+--completed
+
+
+create table todos (
+    id serial primary key,
+    name text,
+    completed boolean
+
+
+
+);
---
+++ seed.sql
@@ -0,0 +1,10 @@
+
+
+insert into todos
+    (name, completed)
+values
+    ('walk the cat', false),        
+    ('repair the hyperdrive', false),
+    ('teturn to Dagobah', false),
+    ('play yugioh with yoda', false),
+    ('buy a flip phone', false);
