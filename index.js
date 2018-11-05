0117710607229f7907d4af8345062a80b1f72086
Isaac Fonseca <isaacfo1010@gmail.com>
2018-11-05 11:09:53 -0500

    added .any

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ const personname = 'Milla'
 console.log(`hello ${personname}`);
 
 console.log('that should not have worked correctly.');
+
+// const pgPromise = require('pg-promise');
+// const pgp = require('pg-promise')();
+
+const pgp = require('pg-promise')();
+const db = pgp({
+    host:'localhost',
+    port: 5432,
+    database:'node-todo-app-db'
+
+});
+
+db.any('select * from todos')
+    .then(results => {
+        console.log(results);
+    })
